0a859ab86be5e2dea24e845e4dbb12033bb0428a
Rohan1194 <rohan_garg@stu.upes.ac.in>
2017-08-21 20:28:15 +0530

    Updated

---
+++ js/index.js
@@ -0,0 +1,36 @@
+var res = [];
+var getdata = [];
+var fs = require("fs");
+var lineReader = require('readline').createInterface({
+   input: require('fs').createReadStream('data.csv')
+});
+
+var myWriteStream = require("fs").createWriteStream("Oilseeds.json")
+
+lineReader.on('line', function(line) {
+    var jsonFromLine = {};
+    var lineSplit = line.split(',');
+    jsonFromLine.Particulars = lineSplit[0];
+    jsonFromLine.Unit = lineSplit[24];
+    res.push(jsonFromLine);
+});
+
+
+lineReader.on('close', function(line) {
+ getdata=res.filter(function(data){
+ 	
+ 	return data.Particulars.includes("Agricultural Production Oilseeds")  
+ });
+ getdata.sort((a,b)=>(b.Unit-a.Unit));
+ console.log(getdata);
+ });
+
+
+lineReader.on('close', function(line) {
+    myWriteStream.write(JSON.stringify(getdata, null, 2))
+});
+
+
+
+
+
